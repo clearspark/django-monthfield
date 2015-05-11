5a3337071e46b1e27989f08446b9b49c4dba3f4b
Matthys <matthysk@clearspark.co.za>
2015-05-11 21:24:10 +0200

    Initial commit

---
+++ month/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+import datetime
+
+def days(days):
+    return datetime.timedelta(days=days)
+
+class Month(object):
+    def __init__(self, year, month):
+        self.year = year
+        self.month = month
+        self._date = datetime.date(year=self.year, month=self.month, day=1)
+
+    @classmethod
+    def from_int(cls, months):
+        y, m = divmod(months, 12)
+        m += 1
+        return cls(y, m)
+
+    @classmethod
+    def from_date(cls, date):
+        return cls(date.year, date.month)
+    
+    @classmethod
+    def from_string(cls, date):
+        y = int(date[:4])
+        m = int(date[5:7])
+        return cls(y, m)
+    def __add__(self, months):
+        return Month.from_int(int(self) + months)
+    def next_month(self):
+        return self + 1
+    def prev_month(self):
+        return self + (-1)
+    def first_day(self):
+        return self._date
+    def last_day(self):
+        return self.next_month().first_day() - days(1)
+    def __int__(self):
+        return self.year * 12 + self.month - 1
+    def __contains__(self, date):
+        return self == date
+    def __eq__(self, x):
+        if isinstance(x, Month):
+            return x.month == self.month and x.year == self.year
+        if isinstance(x, datetime.date):
+            return self.year == x.year and self.month == x.month
+        if isinstance(x, int):
+            return x == int(self)
+        if isinstance(x, basestring):
+            return str(self) == x[:7]
+    def __gt__(self, x):
+        if isinstance(x, Month):
+            if self.year != x.year: return self.year > x.year
+            return self.month > x.month
+        if isinstance(x, datetime.date):
+            return self.first_day() > x
+        if isinstance(x, int):
+            return int(self) > x
+        if isinstance(x, basestring):
+            return str(self) > x[:7]
+    def __ne__(self, x):
+        return not self == x
+    def __le__(self, x):
+        return not self > x
+    def __ge__(self, x):
+        return (self > x) or (self == x)
+    def __lt__(self, x):
+        return not self >= x
+    def __str__(self):
+        return '%s-%02d' %(self.year, self.month)
+    def __unicode__(self):
+        return self.__str__()
+    def __repr__(self):
+        return self.__str__()
+    def datestring(self):
+        return self.first_day().isoformat()
+
+
+class MonthField(models.DateField):
+    description = "A specific month of a specific year."
+    def to_python(self, value):
+        if isinstance(value, Month):
+            month = value
+        elif isinstance(value, datetime.date):
+            month = Month.from_date(value)
+        elif isinstance(value, basestring):
+            month = Month.from_string(value)
+        else:
+            month = None
+        return month
+
+    def get_prep_value(self, value):
+        month = self.to_python(value)
+        if month is not None:
+            return month.first_day()
+        return None
+        
+    def from_db_value(self, value, expression, connection, context):
+        return self.to_python(value)
+
+    def clean(self, value):
+        return self.to_python(value)
+
+class Example(models.Model):
+    name = models.CharField(max_length=20, blank=True)
+    month = MonthField()
+    def __unicode__(self):
+        return unicode(self.month)
---
+++ month/tests.py
@@ -0,0 +1,177 @@
+from django.test import TestCase
+from month.models import Month, Example
+
+import datetime
+# Create your tests here.
+
+def TestMonthFunctions(TestCase):
+    def test_constructors(self):
+        m = Month(2010, 1)
+        self.assertEqual(m.year, 2010)
+        self.assertEqual(m.month, 1)
+
+        m = Month.from_string('2010-01')
+        self.assertEqual(m.year, 2010)
+        self.assertEqual(m.month, 1)
+
+        m = Month.from_date(datetime.date(year=2010, month=1, day=20))
+        self.assertEqual(m.year, 2010)
+        self.assertEqual(m.month, 1)
+    
+    def test_addition(self):
+        m = Month(2010, 1)
+        x = m + 5
+        self.assertEqual(x.year, 2010)
+        self.assertEqual(x.month, 6)
+
+        x = m + 11
+        self.assertEqual(x.year, 2010)
+        self.assertEqual(x.month, 12)
+
+        x = m + 12
+        self.assertEqual(x.year, 2011)
+        self.assertEqual(x.month, 1)
+
+        x = m + 13
+        self.assertEqual(x.year, 2011)
+        self.assertEqual(x.month, 2)
+
+        x = m - 1
+        self.assertEqual(x.year, 2009)
+        self.assertEqual(x.month, 12)
+
+        x = m + 0
+        self.assertEqual(x.year, 2010)
+        self.assertEqual(x.month, 1)
+
+        x = m - 12
+        self.assertEqual(x.year, 2009)
+        self.assertEqual(x.month, 1)
+
+        x = m.next_month()
+        self.assertEqual(x.year, 2010)
+        self.assertEqual(x.month, 2)
+
+        x = m.prev_month()
+        self.assertEqual(x.year, 2009)
+        self.assertEqual(x.month, 12)
+
+    def test_firstday(self):
+        m = Month(2010, 1)
+        self.assertEqual(m.firstDay(), datetime.date(year=2010, month=1, day=1))
+        self.assertEqual(m.last_day(), datetime.date(year=2010, month=1, day=31))
+
+        m = Month(2010, 2)
+        self.assertEqual(m.firstDay(), datetime.date(year=2010, month=2, day=1))
+        self.assertEqual(m.last_day(), datetime.date(year=2010, month=2, day=28))
+
+        m = Month(2008, 2)
+        self.assertEqual(m.firstDay(), datetime.date(year=2008, month=2, day=1))
+        self.assertEqual(m.last_day(), datetime.date(year=2008, month=2, day=29))
+
+    def test_contains(self):
+        m = Month(2010, 1)
+        assert datetime.date(year=2010, month=1, day=1) in m
+        assert datetime.date(year=2010, month=1, day=10) in m
+        assert datetime.date(year=2010, month=1, day=31) in m
+        assert datetime.date(year=2010, month=2, day=1) not in m
+        assert datetime.date(year=2009, month=12, day=31) not in m
+        assert datetime.date(year=2009, month=1, day=31) not in m
+        assert datetime.date(year=2010, month=2, day=15) not in m
+    
+    def test_int_conversion(self):
+        m = Month(2010, 1)
+        n = Month.from_int(int(m))
+        self.assertEqual(n.year, 2010)
+        self.assertEqual(n.month, 1)
+
+    def test_comparisons(self):
+        m = Month(2010, 1)
+        assert m == "2010-01-20"
+        assert m == "2010-01-20"
+        assert m == "2010-01"
+        assert m == "2010-01-20"
+        assert m < "2010-02-01"
+        assert m > "2009-12"
+        assert m > "2009-12-31"
+        
+        p = m.prev_month()
+        n = m.next_month()
+
+        assert m == m
+        assert m <= m
+        assert m >= m
+        assert not m > m
+        assert not m < m
+        assert not m != m
+
+        assert not m == p
+        assert m > p
+        assert m >= p
+        assert not m <= p
+        assert not m < p
+        assert m != p
+
+        assert not m == n
+        assert m != n
+        assert m < n
+        assert m <= n
+        assert not m > n
+        assert not m >= n
+
+class test_model_field(TestCase):
+    def test_queries(self):
+        e = Example(name='2010-01', month=Month(2010, 1))
+        e.save()
+        assert isinstance(e.month, Month)
+        assert e.month.month == 1
+        assert e.month.year == 2010
+        pk = e.pk
+        
+        e = Example.objects.get(pk=pk)
+        assert isinstance(e.month, Month)
+        assert e.month.month == 1
+        assert e.month.year == 2010
+
+
+        e = Example(name='2010-01', month='2010-01')
+        e.save()
+        pk = e.pk
+        
+        e = Example.objects.get(pk=pk)
+        assert isinstance(e.month, Month)
+        assert e.month.month == 1
+        assert e.month.year == 2010
+
+        e = Example(name='2010-01', month=datetime.date(year=2010, month=1, day=20))
+        e.save()
+        pk = e.pk
+        
+        e = Example.objects.get(pk=pk)
+        assert isinstance(e.month, Month)
+        assert e.month.month == 1
+        assert e.month.year == 2010
+
+        Example.objects.all().delete()
+        for year in range(2001, 2011):
+            for month in range(1, 13):
+                name = "%s - %02d" %(year, month)
+                Example(name=name, month=Month(year, month)).save()
+
+        qs = Example.objects.filter(month='2005-12')
+        assert qs.exists()
+        assert qs.count() == 1
+
+        qs = Example.objects.filter(month__gte='2005-12')
+        assert qs.exists()
+        self.assertEqual(qs.count(), 61)
+
+        qs = Example.objects.filter(month__gt='2005-12')
+        assert qs.exists()
+        assert qs.count() == 60
+    def tearDown(self):
+        Example.objects.all().delete()
+
+
+
+
