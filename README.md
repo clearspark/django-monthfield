c213baf81ee6780790760453cc6e04209ec06e0f
Matthys <matthysk@clearspark.co.za>
2015-05-11 23:28:27 +0200

    Add widget for form input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # django-monthfield
 Custom field for storing month (YYYY-MM) as a django field.
+
+For test_site:
+user: test
+pass: test
---
+++ month/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from month.widgets import MonthSelectorWidget
+
+class MonthField(forms.DateField):
+    widget = MonthSelectorWidget
+
+
+
+
