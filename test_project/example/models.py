42251f59bbe6519f4d98796a6be5f7cceb23f57b
Alex Sandro <geniofuturo@gmail.com>
2017-01-26 14:44:04 -0200

    Configuring Test Attributes.

--- test_project/example/models.py
+++ test_project/example/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from month.models import MonthField
 
+
 # Create your models here.
 
 class Example(models.Model):
     name = models.CharField(max_length=20, blank=True)
-    month = MonthField()
+    month = MonthField("Month Value", help_text="some help...")
+
     def __unicode__(self):
         return unicode(self.month)
