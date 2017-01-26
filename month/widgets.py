be5260d45176cea1cb1c3ea87c56daaa2f21f875
Alex Sandro <geniofuturo@gmail.com>
2017-01-26 10:37:00 -0200

    pep8 (reformat).

--- month/widgets.py
+++ month/widgets.py
@@ -5,12 +5,13 @@ https://docs.djangoproject.com/en/1.8/ref/forms/widgets/#base-widget-classes
 from datetime import date
 from django.forms import widgets
 
+
 class MonthSelectorWidget(widgets.MultiWidget):
     def __init__(self, attrs=None):
         # create choices for days, months, years
         # example below, the rest snipped for brevity.
         years = [(year, year) for year in range(1980, 2020)]
-        months = [ (month, month) for month in range(1, 13)]
+        months = [(month, month) for month in range(1, 13)]
         _widgets = (
             widgets.Select(attrs=attrs, choices=months),
             widgets.Select(attrs=attrs, choices=years),
@@ -22,9 +23,9 @@ class MonthSelectorWidget(widgets.MultiWidget):
             if isinstance(value, basestring):
                 m = int(value[5:7])
                 y = int(value[:4])
-                return [ m, y ]
+                return [m, y]
             return [value.month, value.year]
-        return [ None, None]
+        return [None, None]
 
     def format_output(self, rendered_widgets):
         return ''.join(rendered_widgets)
@@ -35,7 +36,7 @@ class MonthSelectorWidget(widgets.MultiWidget):
             for i, widget in enumerate(self.widgets)]
         try:
             D = date(day=1, month=int(datelist[0]),
-                    year=int(datelist[1]))
+                     year=int(datelist[1]))
         except ValueError:
             return ''
         else:
