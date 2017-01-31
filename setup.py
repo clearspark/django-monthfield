0179e5274622bf0bef4aebaeaba17972a46789b4
Alex Sandro <geniofuturo@gmail.com>
2017-01-31 12:18:04 -0200

    Increasing the version (0.1.0).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-monthfield',
-    version='0.0.1',
+    version='0.1.0',
     author=u'Matthys Kroon',
     author_email='matthysk@clearspark.co.za',
     packages=['month'],
