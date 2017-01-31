98ed8ca3441ac5cb73e327a699ece4204a479a28
Alex Sandro <geniofuturo@gmail.com>
2017-01-31 12:17:04 -0200

    Fixing the app install.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 '''Still needs some work to properly include the test_site and sqlite db'''
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup
+
 setup(
     name='django-monthfield',
     version='0.0.1',
     author=u'Matthys Kroon',
     author_email='matthysk@clearspark.co.za',
-    packages=find_packages(),
+    packages=['month'],
     include_package_data=True,
     url='https://github.com/clearspark/django-monthfield',
     license='BSD licence, see LICENCE',
     description='Provides a field for storing months (YYYY-MM) on django models.',
     long_description=open('README.md').read(),
     zip_safe=False,
-    )
+)
