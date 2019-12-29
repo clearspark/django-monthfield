4ad7a566db85d56c9a9f6fae5bbe46a744a57db0
Matthys <matthysk@gmail.com>
2019-12-29 10:04:00 +0200

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-monthfield',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Matthys Kroon',
     author_email='matthysk@clearspark.co.za',
     packages=['month'],
